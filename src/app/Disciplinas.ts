908c7cff33fcbddaa0689cc73f0ecfc1b0113063
LucasCelestino <lucasrashido@gmail.com>
2022-04-01 21:30:32 -0300

    2 files modified, 1 added

--- src/app/Disciplinas.ts
+++ src/app/Disciplinas.ts
@@ -2,70 +2,29 @@ export const disciplinasList = [
   {
     id: 1,
     nome: 'Desenvolvimento para Servidores II',
-    dia: 'Quinta-Feira',
-    horario: '19:00',
-    ementa:
-      'Conceitos e evolução das tecnologias de programação de servidores. Recursos da linguagem escolhida para servidores na Internet. Padrões de projetos. Integração com sistemas (Google Maps API, Twitter, entre outros)',
-    objetivo:
-      'Desenvolver um site completo de e-commerce ou outro tipo de negócio na Internet usando uma linguagem apropriada a servidores, banco de dados e padrões de projeto.',
   },
   {
     id: 2,
     nome: 'Desenvolvimento para dispositivos móveis I',
-    dia: 'Segunda-Feira',
-    horario: '19:00',
-    ementa:
-      'Introdução e conceitos. Recursos disponíveis para dispositivos móveis na linguagem de marcação de textos. Uso de algum ambiente de desenvolvimento: configuração do ambiente; emulação de dispositivos móveis nos computadores pessoais; interface gráfica; serviços disponíveis; banco de dados nos dispositivos. Desenvolvimento de aplicações que utilizam dados de geolocalização. Integração entre sistemas remotos.',
-    objetivo:
-      'Apresentar as tecnologias de desenvolvimento de aplicativos para dispositivos móveis.',
   },
   {
     id: 3,
     nome: 'Inglês V',
-    dia: 'Terça-Feira',
-    horario: '19:00',
-    ementa:
-      'Aprofundamento da compreensão e produção oral e escrita por meio da integração das habilidades linguístico-comunicativas. Ênfase na oralidade, atendendo às especificidades acadêmicoprofissionais da área e abordando aspectos socioculturais da língua inglesa.',
-    objetivo:
-      'Apresentar os recursos do idioma necessários à compreensão e produção oral e escrita por meio da integração das habilidades linguístico-comunicativas. Apoiar as demais atividades dos estudantes na criação de sítios Internet.',
   },
   {
     id: 4,
     nome: 'Negócios e Marketing Eletrônicos',
-    dia: 'Quarta-Feira',
-    horario: '19:00',
-    ementa:
-      'Conceitos de marketing e de marketing digital. O comportamento do consumidor on-line. Marketing de conteúdo. Marketing nas mídias sociais. E-mail marketing. Marketing viral. Publicidade on-line. Pesquisa de marketing on-line. Marketing e os negócios na Internet. Estudos de caso.',
-    objetivo:
-      'Apresentar e discutir as questões do marketing na era da Internet. Abordar as principais estratégias, ações, ferramentas e mídias presentes na Web.',
   },
   {
     id: 5,
     nome: 'Projeto de prototipagem e testes de Usabilidade',
-    dia: 'Sábado',
-    horario: '08:00',
-    ementa:
-      'Conceitos, métodos de prototipagem e ferramentas de teste de protótipos. Conceitos de usabilidade e estratégias de design. Métodos de teste de usabilidade. Planejamento de teste de usabilidade. Recrutamento. Projeto de usabilidade. Plano de teste. Sessão de teste de usabilidade. Análise de dados. Comunicação dos resultados. Variação de técnicas.',
-    objetivo:
-      'Aprofundar os conceitos de design da usabilidade e métodos de ensaio para mídia interativa. Usabilidade baseada na adaptação de design de interface para uma experiência do usuário mais eficaz. Apresentar e usar diferentes métodos e ferramentas de prototipagem para explorar com os colegas de equipe ou clientes: conceitos de design, comunicação e testes de usabilidade.',
   },
   {
     id: 6,
     nome: 'Projeto do trabalho de graduação em Sistemas',
-    dia: 'Sábado',
-    horario: '19:00',
-    ementa:
-      'Conceito de portfólio no âmbito da educação superior. Planejamento e organização de portfólios.',
-    objetivo:
-      'Apoiar o estudante na organização de seu portfólio de projetos desenvolvidos ao longo do curso.',
   },
   {
     id: 7,
     nome: 'Tópicos Especiais em Sistemas para Internet II',
-    dia: 'Sexta-feira',
-    horario: '11:30',
-    ementa: 'Recursos e aplicações da tecnologia escolhida.',
-    objetivo:
-      'Apresentar tecnologias ainda não estudadas ou aprofundar algum tema já visto definido pela coordenadoria do curso em cada unidade.',
   },
 ];
