87edf80edc7cd68edb4a9b2c64b13344a1c609e8
LucasCelestino <lucasrashido@gmail.com>
2022-04-01 21:58:35 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -13,6 +14,7 @@ import { DisciplinaService } from './disciplina.service';
 @NgModule({
   imports: [
     BrowserModule,
+    CommonModule,
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
