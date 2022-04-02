7fb217d288b05db4135d7a929bc6b77213fab689
LucasCelestino <lucasrashido@gmail.com>
2022-04-01 21:04:58 -0300

    2 files modified, 1 added, 1 deleted

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { TemporizadorService } from '../temporizador.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private temporizador: TemporizadorService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
